043b93ea98b7046a7d886feab3b44fbd5a9b549c
lynnagidza <lynnagidza@gmail.com>
2023-01-29 00:21:18 +0300

    corrections

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""This program will assign a random signed number to the variable number each time it is executed.
-Print whether the number stored in the variable number is positive or negative"""
+"""This program will assign a random signed number to the variable number each
+time it is executed. Print whether the number stored in the variable number is
+positive or negative"""
 
 import random
 number = random.randint(-10, 10)
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""This program will assign a random signed number to the variable number each time it is executed.
+"""This program will assign a random signed number to the variable number each
+time it is executed.
 Print the last digit of the number stored in the variable number"""
 
 import random
 number = random.randint(-10000, 10000)
 last_digit = int(repr(number)[-1])
 # YOUR CODE HERE
+if number < 0:
+    last_digit = -last_digit
 if last_digit > 5:
     appended_string = "and is greater than 5"
 if last_digit == 0:
