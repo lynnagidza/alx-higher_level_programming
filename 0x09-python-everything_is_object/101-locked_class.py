217bb2c34c8d8e2f86a0a30b31c0a33046cbbcc7
lynnagidza <lynnagidza@gmail.com>
2023-06-29 12:32:45 +0300

    coorection

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 """
 101-locked_class module
-
 This module defines the LockedClass that prevents dynamically creating new
 instance attributes,
 except for 'first_name'.
--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def copy_list(lst):
     """Returns a copy of a list."""
     return lst[:]
